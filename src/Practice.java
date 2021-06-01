a2657ad6095225064c60924a153ed060ef095672
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2021-06-01 17:31:35 +0700

    fix matrixFill() add matrixCol() to calculate column elements

--- src/Practice.java
+++ src/Practice.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Practice {
     private static int[] myArray = {10, 4, 6, 7, 8, 0, 0, 0, 0, 0};
+
     // Main
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -35,15 +36,19 @@ public class Practice {
 //        removeFromArray(x, myArray);
 //        System.out.println(Arrays.toString(myArray));
 
-        // Matrix Calculation
+        // Matrix Generation
         int[][] myMatrix = matrixFill();
-        int result = matrixCalc(myMatrix);
-        System.out.println("Result: " + result);
+        // Calculate diagonal elements
+        // int result1 = matrixCalc(myMatrix);
+        // Calculate sum of column elements
+        System.out.println("Enter the column index: ");
+        int colIndex = Integer.parseInt(sc.nextLine());
+        int result2 = matrixCol(myMatrix, colIndex);
+        System.out.println("Result: " + result2);
     }
 
 
-
-     //Check if a number is prime
+    //Check if a number is prime
     public static boolean isPrime(int number) {
         if (number == 2) {
             System.out.println(number + " is a prime.");
@@ -64,7 +69,7 @@ public class Practice {
 
     // Print all prime numbers < 100
     public static boolean checkPrime(int x) {
-        if(x != 2) {
+        if (x != 2) {
             for (int i = 2; i < Math.sqrt(x); i++) {
                 if (x % i == 0) {
                     return false;
@@ -73,9 +78,10 @@ public class Practice {
         }
         return true;
     }
-    public static void printPrime(){
+
+    public static void printPrime() {
         for (int i = 2; i < 101; i++) {
-            if (checkPrime(i)){
+            if (checkPrime(i)) {
                 System.out.println(i);
             }
         }
@@ -87,9 +93,9 @@ public class Practice {
         int[] results = new int[numb];
         int i = 0;
         int N = 2;
-        while (count < numb){
-            if (checkPrime(N)){
-                count ++;
+        while (count < numb) {
+            if (checkPrime(N)) {
+                count++;
                 results[i++] = N;
             }
             N++;
@@ -100,15 +106,14 @@ public class Practice {
 
 
     // Insert a new value to an array
-    public static int[] insertIntoArray(int x, int index){
+    public static int[] insertIntoArray(int x, int index) {
         // int[] myArray = {10, 4, 6, 7, 8, 0, 0, 0, 0, 0};
         // {10, 4, 32, 6, 7, 8, 0, 0, 0, 0}
-        if (index <= 0 && index >= myArray.length-1){
+        if (index <= 0 && index >= myArray.length - 1) {
             System.out.println("Cannot insert number");
-        }
-        else{
-            for (int i = myArray.length-1; i > index; i--) {
-                myArray[i] = myArray[i-1];
+        } else {
+            for (int i = myArray.length - 1; i > index; i--) {
+                myArray[i] = myArray[i - 1];
             }
             myArray[index] = x;
         }
@@ -116,13 +121,13 @@ public class Practice {
     }
 
     // Remove from array
-    public static void removeFromArray(int x, int[] someArray){
+    public static void removeFromArray(int x, int[] someArray) {
         int index_del = 0;
         for (int i = 0; i < someArray.length; i++) {
             if (someArray[i] == x) {
                 index_del = i;
-                for (int j = index_del; j < someArray.length-1; j++) {
-                    someArray[j] = someArray[j+1];
+                for (int j = index_del; j < someArray.length - 1; j++) {
+                    someArray[j] = someArray[j + 1];
                 }
             }
         }
@@ -130,26 +135,46 @@ public class Practice {
 
     // 2D array
     // Get user to input a 2D array
-    public static int[][] matrixFill(){
-        System.out.println("Enter the matrix elements: ");
+    public static int[][] matrixFill() {
         Scanner newSc = new Scanner(System.in);
-        int[][] aMatrix = new int[2][2];
-        for (int row = 0; row < 2; row++) {
-            for (int col = 0; col < 2; col++) {
-                aMatrix[row][col] = Integer.parseInt(newSc.nextLine());
+        System.out.println("Enter the number of rows: ");
+        int row = Integer.parseInt(newSc.nextLine());
+        System.out.println("Enter the number of columns: ");
+        int col = Integer.parseInt(newSc.nextLine());
+        int[][] aMatrix = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.println("Enter a matrix elements: ");
+                aMatrix[i][j] = Integer.parseInt(newSc.nextLine());
             }
         }
         return aMatrix;
     }
+
     // Calculate the diagonal elements
-    public static int matrixCalc(int[][] someMatrix){
+    public static int matrixCalc(int[][] someMatrix) {
         int sum = 0;
         for (int i = 0; i < someMatrix.length; i++) {
-                sum += someMatrix[i][i];
-            }
-        return sum;
+            sum += someMatrix[i][i];
         }
+        return sum;
     }
+
+    // Calculate the element of a matrix column
+    public static int matrixCol(int[][] anotherMatrix, int colIndex){
+        int colSum = 0;
+        for (int i = 0; i < anotherMatrix.length; i++) {
+            colSum += anotherMatrix[i][colIndex-1];
+        }
+        return colSum;
+    }
+
+
+}
+
+
+
+
 
 
 
